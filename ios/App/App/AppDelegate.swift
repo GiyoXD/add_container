1c5cab87ad4977d4686242122dbda59dd99b8673
GiyoXD <benloy126@gmail.com>
2026-09-05 15:11:01 +0700

    fix(ios): resolve continue userActivity signature incompatibility

--- ios/App/App/AppDelegate.swift
+++ ios/App/App/AppDelegate.swift
@@ -43,7 +43,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the app was launched with an activity, including Universal Links.
         // Feel free to add additional processing here, but if you want the App API to support
         // tracking app url opens, make sure to keep this call
-        return ApplicationDelegateProxy.shared.application(application, continue: userActivity, restorationHandler: restorationHandler)
+        #if canImport(Capacitor)
+        if userActivity.activityType == NSUserActivityTypeBrowsingWeb, let url = userActivity.webpageURL {
+            return ApplicationDelegateProxy.shared.application(application, open: url, options: [:])
+        }
+        #endif
+        return false
     }
 
 }
